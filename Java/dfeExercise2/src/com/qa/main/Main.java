039f9bd51cc9942b5c94282e2d61b757dd777d6c
David Cull <nowe2111@yahoo.co.uk>
2021-10-19 11:36:11 +0100

    update Exercise 2 Main file

--- Java/dfeExercise2/src/com/qa/main/Main.java
+++ Java/dfeExercise2/src/com/qa/main/Main.java
@@ -3,10 +3,10 @@ package com.qa.main;
 public class Main {
 
 	public static void main(String[] args) {
+		//Exercise 2, Calling methods from another Class
 		Demo.pizzaTopping();
 		Demo.aWord();
 		Demo.aNumber();
-
 	}
 
 }
