acdc8269f1f3c44b56ed018c60c955d34c9e88c7
David Cull <nowe2111@yahoo.co.uk>
2021-11-02 11:47:20 +0000

    updating GarageSpring adding missing libraries

--- Spring/GarageSpring/src/test/java/integration/IntegrationTest.java
+++ Spring/GarageSpring/src/test/java/integration/IntegrationTest.java
@@ -1,7 +1,6 @@
 package integration;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -10,15 +9,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 //import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.ResultHandler;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.SpringGarage.Cull_David.GarageSpringApplication;
